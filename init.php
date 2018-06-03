ea29a1ef3316880af9fd433aed8976789ff4734a
CorentinCrz <ccroiz@live.fr>
2018-06-03 18:06:01 +0200

    FrontController

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Corentin
+ * Date: 03/06/2018
+ * Time: 17:57
+ */
+
+define("KANDT_ROOT_DIR", __DIR__);
+define("KANDT_ACTION_PARAM", "a");
+define("KANDT_ROOT_URI", "?");
---
+++ src/Controller/PageController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Corentin
+ * Date: 03/06/2018
+ * Time: 18:03
+ */
+
+namespace Controller;
+
+
+class PageController
+{
+
+}
